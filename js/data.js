6f7d43c97de232938a0db3f9b2162bd9190a50d9
daniel salgado <daniel.salgado.p@gmail.com>
2020-12-07 20:36:05 +0100

    added fresa

--- js/data.js
+++ js/data.js
@@ -7,10 +7,18 @@ let data =
         "calories": 100,
         "vitamins": ['A1', 'B12']
     },
-    "sandia": {
-        "info": "la sandia ayuda al corazón y aplaza el envejecimiento. A continuación, te comentamos cuáles son los beneficios de consumir sandía, dentro de una dieta equilibrada: 1. Mejora la ... La sandía se consume generalmente en crudo, en rodajas, cuartos o incluso en bolas. También se emplea para elaborar sorbetes, purés, mermeladas y confituras. En Rusia preparan un vino muy popular a partir de zumo de sandía. La corteza se aprovecha a veces encurtida o confitada y en los medios rurales sirve como alimento del ganado. Las semillas se consumen en algunas regiones tostadas y saladas.",
-        "img": "img/sandia.png",
+    "fresa": {
+        "id" : "fresa",
+        "info": "Son una fuente de vitaminas del grupo B, como la vitamina B6, la niacina, la riboflavina, el ácido pantoténico o el ácido fólico. -Actúa como un potente antioxidante y antiinflamatorio natural. Una taza de fresas contiene 21 gramos de manganeso.",
+        "img": "img/fresa.jpg",
         "calories": 200,
+        "vitamins": ['K', 'D']
+    },
+    "sandia": {
+        "id" : "sandia",
+        "info": "la sandia ayuda al corazón y aplaza el envejecimiento. A continuación, te comentamos cuáles son los beneficios de consumir sandía, dentro de una dieta equilibrada: 1. Mejora la ... La sandía se consume generalmente en crudo, en rodajas, cuartos o incluso en bolas. También se emplea para elaborar sorbetes, purés, mermeladas y confituras. En Rusia preparan un vino muy popular a partir de zumo de sandía. La corteza se aprovecha a veces encurtida o confitada y en los medios rurales sirve como alimento del ganado. Las semillas se consumen en algunas regiones tostadas y saladas.",
+        "img": "img/sandia.jpg",
+        "calories": 300,
         "vitamins": ['A1', 'C']
     }
 }
