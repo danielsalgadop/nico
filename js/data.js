57b28fa17913d471f4f4a211d532d80bb74410fa
daniel salgado <daniel.salgado.p@gmail.com>
2020-12-07 19:38:47 +0100

    using data from data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,17 @@
-let data  = "Como su nombre sugiere, la dieta de la manzana es un régimen de alimentación en el que esta fruta cobra un rol importante";
+let data =
+{
+    "manzana": {
+        "id" : "manzana",
+        "info": "Como su nombre sugiere, la dieta de la manzana es un régimen de alimentación en el que esta fruta cobra un rol importante",
+        "img": "img/manzana.png",
+        "calories": 100,
+        "vitamins": ['A1', 'B12']
+    },
+    "sandia": {
+        "info": "la sandia ayuda al corazón y aplaza el envejecimiento. A continuación, te comentamos cuáles son los beneficios de consumir sandía, dentro de una dieta equilibrada: 1. Mejora la ... La sandía se consume generalmente en crudo, en rodajas, cuartos o incluso en bolas. También se emplea para elaborar sorbetes, purés, mermeladas y confituras. En Rusia preparan un vino muy popular a partir de zumo de sandía. La corteza se aprovecha a veces encurtida o confitada y en los medios rurales sirve como alimento del ganado. Las semillas se consumen en algunas regiones tostadas y saladas.",
+        "img": "img/sandia.png",
+        "calories": 200,
+        "vitamins": ['A1', 'C']
+    }
+}
+;
--- js/router.js
+++ js/router.js
@@ -1,12 +1,17 @@
 page = 'manzana';
 
 
-function fillAlert(alertSelector) {
+function fillAlert(alertSelector,productName) {
+
+    // using string (productName) as property
+    let product = data[productName];
+    console.log(product);
+
     alert = '<div class="alert alert-success" role="alert">';
-    alert += fillAlertHeading('manzana');
-    alert += fillAlertContent();
+    alert += fillAlertHeading(productName);
+    alert += fillAlertContent(product);
     alert += '<hr>';
-    alert += fillAlertFinal();
+    alert += fillAlertCaloriesAndVitamins(product);
     alert += '</div>';
     alertSelector.innerHTML = alert;
 }
@@ -15,13 +20,13 @@ function fillAlertHeading(headingContent) {
     return `<h4 class="alert-heading">${headingContent}</h4>`;
 }
 
-function fillAlertContent() {
-    // funciona igual con sin atributo 'draggable' ??
-    return '<div><img id="manzana" draggable="true" ondragstart="drag(event)" src="img/manzana.png"></div><p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>';
+function fillAlertContent(product) {
+    // funciona igual con y sin atributo 'draggable' ??
+    return '<div><img id="'+product.id+'" draggable="true" ondragstart="drag(event)" src="'+product.img+'"></div><p>'+product.info+'</p>';
 }
 
-function fillAlertFinal() {
-    return '<p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>';
+function fillAlertCaloriesAndVitamins(product) {
+    return '<p class="mb-0"><ul><li>Calories = '+product.calories+'</li><li>Vitamins = '+product.vitamins+'</li></ul></p>';
 }
 
-fillAlert(document.getElementById('main'));
+
