c3519f180e20b7a9d21fab825446036b6bbe6b35
daniel salgado <daniel.salgado.p@gmail.com>
2020-12-07 20:48:14 +0100

    added kiwi and melocoton

--- js/data.js
+++ js/data.js
@@ -14,6 +14,21 @@ let data =
         "calories": 200,
         "vitamins": ['K', 'D']
     },
+    "melocoton": {
+        "id" : "melocoton",
+        "info": "El melocotón nos aporta calcio, potasio, fósforo, hierro, manganeso y flavonoides. También es fuente de vitaminas A, B1, B2 y C (óptima para la absorción del hierro)",
+        "img": "img/melocoton.jpg",
+        "calories": 400,
+        "vitamins": ['A1', 'B1','B2','C']
+    },
+    "kiwi": {
+        "id" : "kiwi",
+        "info": "    El kiwi es una baya procedente de China que se introdujo en los años 70 en el mercado mundial. Con un sabor característico y potente, el kiwi guarda algunos beneficios y propiedades desconocidos para gran parte de sus consumidores. Además, el kiwi contiene cantidades destacables de vitamina K, potasio, ácido fólico y fibra, enumera la especialista.",
+        "img": "img/kiwi.jpg",
+        "calories": 400,
+        "vitamins": ['A1']
+    },
+
     "sandia": {
         "id" : "sandia",
         "info": "la sandia ayuda al corazón y aplaza el envejecimiento. A continuación, te comentamos cuáles son los beneficios de consumir sandía, dentro de una dieta equilibrada: 1. Mejora la ... La sandía se consume generalmente en crudo, en rodajas, cuartos o incluso en bolas. También se emplea para elaborar sorbetes, purés, mermeladas y confituras. En Rusia preparan un vino muy popular a partir de zumo de sandía. La corteza se aprovecha a veces encurtida o confitada y en los medios rurales sirve como alimento del ganado. Las semillas se consumen en algunas regiones tostadas y saladas.",
